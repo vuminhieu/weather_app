2a016ab2434a2927187d7e414c6d74fd8abc7e51
MinhHieuVux <minhhieuvux@gmail.com>
2022-10-29 09:26:08 +0700

    fix layout

--- src/script.js
+++ src/script.js
@@ -5,142 +5,128 @@ const accessKeyApiLocate = "pk.eyJ1IjoiYW5ocXQxIiwiYSI6ImNsOWI4c2F3ZjB5d2Mzdm8za
 const APILocate = `https://api.mapbox.com/geocoding/v5/mapbox.places/${Add}.json?access_token=${accessKeyApiLocate}`
 
 let weather = {
-  apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { latitude } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Thời tiết tại " + name;
-    // console.log("Ten" + name);
-    // console.log("Kinh do" + latitude);
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-
-    document.querySelector(".humidity").innerText =
-      " Lượng mưa : " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Tốc độ gió: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-    // console.log(value)
-  },
+    apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        document.querySelector(".city").innerText = "Thời tiết tại " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = " Lượng mưa : " + humidity + "%";
+        document.querySelector(".wind").innerText = "Tốc độ gió: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+        // console.log(value)
+    },
 };
-
-
 
 /* Fetching Data from OpenCageData Geocoder */
 let geocode = {
-  reverseGeocode: function (latitude, longitude) {
-    var apikey = "90a096f90b3e4715b6f2e536d934c5af";
+    reverseGeocode: function (latitude, longitude) {
+        var apikey = "90a096f90b3e4715b6f2e536d934c5af";
+        var api_url = "https://api.opencagedata.com/geocode/v1/json";
+        var request_url =
+            api_url +
+            "?" +
+            "key=" +
+            apikey +
+            "&q=" +
+            encodeURIComponent(latitude + "," + longitude) +
+            "&pretty=1" +
+            "&no_annotations=1";
 
-    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+        var request = new XMLHttpRequest();
+        request.open("GET", request_url, true);
 
-    var request_url =
-      api_url +
-      "?" +
-      "key=" +
-      apikey +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
+        request.onload = function () {
 
-    var request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
+            if (request.status == 200) {
+                var data = JSON.parse(request.responseText);
+                weather.fetchWeather(data.results[0].components.city);
+                // console.log(data.results[0].components.city)
+            } else if (request.status <= 500) {
+                console.log("unable to geocode! Response code: " + request.status);
+                var data = JSON.parse(request.responseText);
+                console.log("error msg: " + data.status.message);
+            } else {
+                console.log("server error");
+            }
+        };
 
-    request.onload = function () {
+        request.onerror = function () {
+            console.log("unable to connect to server");
+        };
 
-      if (request.status == 200) {
-        var data = JSON.parse(request.responseText);
-        weather.fetchWeather(data.results[0].components.city);
-        // console.log(data.results[0].components.city)
-      } else if (request.status <= 500) {
+        request.send();
+    },
+    getLocation: function () {
+        function success(data) {
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        }
 
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-
-    request.onerror = function () {
-      console.log("unable to connect to server");
-    };
-
-    request.send();
-  },
-  getLocation: function() {
-    function success (data) {
-      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, console.error);
+        } else {
+            weather.fetchWeather("Manipal");
+        }
     }
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, console.error);
-    }
-    else {
-      weather.fetchWeather("Manipal");
-    }
-  }
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
+    weather.search();
 });
 
 document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
 
 weather.fetchWeather("Ha Noi");
-
-
 
 geocode.getLocation();
 const test = document.getElementById("22")
 
-  const test1 = document.getElementById("44")
-  test1.addEventListener("click", () => {
+const test1 = document.getElementById("44")
+test1.addEventListener("click", () => {
     const valueInput = test.value
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${valueInput}.json?access_token=${accessKeyApiLocate}`, {
-      method: "GET",
-    }).then((res) => res.json()) .then((data) => console.log(data.features))
-  })
+        method: "GET",
+    }).then((res) => res.json()).then((data) => console.log(data.features))
+})
 
 test.addEventListener("keyup", (event) => {
-  if (event.key == "Enter") {
-    const valueInput = test.value
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${valueInput}.json?access_token=${accessKeyApiLocate}`, {
-      method: "GET",
-    }).then((res) => res.json()) .then((data) => console.log(data.features))
-  }
+    if (event.key == "Enter") {
+        const valueInput = test.value
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${valueInput}.json?access_token=${accessKeyApiLocate}`,
+            {
+                method: "GET",
+            }).then((res) => res.json()).then((data) => console.log(data.features))
+    }
 
 })
 
